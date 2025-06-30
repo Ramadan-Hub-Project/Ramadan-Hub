35aaaac472f82327f26279b2068c6f119c694712
jamila <jamiska07@gmail>
2025-06-30 21:22:48 +0300

    update Contact and Dark mode

--- JS/script.js
+++ JS/script.js
@@ -250,3 +250,17 @@ function displayCardHadith() {
 displayCardHadith()
 
 // Hadiths Ends Here
+
+
+var icon = document.querySelector("#icon");
+
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme")
+    if(document.body.classList.contains("dark-theme")){
+        icon.classList = "fa-solid fa-brightness";
+    } 
+  }
+
+
+
+
