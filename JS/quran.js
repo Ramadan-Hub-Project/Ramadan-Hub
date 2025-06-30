728222637cc87b760af8f67abbbabd538e21eb1f
unknown <khalidhassanmohammed06@gmail.com>
2025-06-30 19:55:00 +0300

    Navigation it's doen!

--- JS/quran.js
+++ JS/quran.js
@@ -129,3 +129,23 @@ displayCardHadith()
 
 // Hadiths Ends Here
 
+const myButton = document.querySelector("#btn");
+myButton.addEventListener("click", function() {
+    window.location.href = "sing-in.html";
+});
+
+// Side Bar Navigation Starts Here
+
+const menuBtn = document.querySelector("#menuBtn");
+const closeBtn = document.querySelector("#closeBtn");
+const menuBarBtn = document.querySelector("#menuBarBtn");
+
+menuBtn.addEventListener("click", () => {
+    menuBarBtn.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    menuBarBtn.classList.remove("active");
+});
+
+// Side Bar Navigation Ends Here
